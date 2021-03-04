6dcbc1a1521eb34d2c00cf287d28610161d7c571
brunosacom <bruno.sa.com@gmail.com>
2021-03-04 20:20:53 -0300

    teste01

---
+++ php/dropdown_ajax/data.php
@@ -0,0 +1,24 @@
+<?php 
+require $_SERVER['DOCUMENT_ROOT']."/php/mysqli_connect.php";
+
+$paisid = $_POST['ps'];   // pais id
+
+$sql = "SELECT uf_id, uf_alpha2, uf_nome FROM _uf";
+
+$result = mysqli_query($con,$sql);
+
+$uf_arr = array();
+
+while( $row = mysqli_fetch_array($result) ){
+    $ufid = $row['uf_id'];
+    $uf_nome = $row['uf_nome'];
+
+    $uf_arr[] = array("<option value=\"".$ufid."\" >".$uf_nome."</option>");
+}
+
+// encoding array to json format
+//echo json_encode($uf_arr);
+
+echo $uf_arr;
+
+?>
---
+++ php/dropdown_ajax/index.php
@@ -0,0 +1,56 @@
+<?php 
+require $_SERVER['DOCUMENT_ROOT']."/php/mysqli_connect.php";
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>DropDown using Ajax JS</title>
+</head>
+
+<body>
+    <form action="">
+    <div>paises</div>
+    <select id="sel_pais" onchange="showUF(this.value)">
+        <option value="0" SELECTED>- Select -</option>
+        <?php 
+        // Fetch Department
+        $sql_pais = "SELECT * FROM _pais ORDER BY pais_ptbr ASC";
+        $pais_data = mysqli_query($con,$sql_pais);
+        while($row = mysqli_fetch_assoc($pais_data) ){
+            $paisid = $row['pais_numero'];
+            $pais_ptbr = $row['pais_ptbr'];
+            
+            // Option
+            echo "<option value=\"".$paisid."\" >".$pais_ptbr."</option>";
+        }
+        ?>
+    </select>
+    <div class="clear"></div>
+
+    <div>unidades federativas</div>
+    <select id="sel_uf">
+        <option value="0">- Select -</option>
+    </select>
+    </form>
+</body>
+
+<script>
+    function showUF(str) {
+  var xhttp;
+  if (str == "") {
+    document.getElementById("sel_uf").innerHTML = "";
+    return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("sel_uf").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "data.php?ps="+str, true);
+  xhttp.send();
+}
+</script>
+</html>
