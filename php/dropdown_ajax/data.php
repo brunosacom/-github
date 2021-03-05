230108bf510903098f29645b908f40d3911af492
brunosacom <bruno.sa.com@gmail.com>
2021-03-05 11:35:21 -0300

    mais um

--- php/dropdown_ajax/data.php
+++ php/dropdown_ajax/data.php
@@ -10,8 +10,8 @@ $sql = "SELECT uf_id, uf_alpha2, uf_nome FROM _uf WHERE pais_numero = ?";
 $stmt = $con->prepare($sql);
 $stmt->bind_param("s", $_GET['p']);
 $stmt->execute();
-$stmt->store_result();
-$stmt->bind_result($uf_id, $uf_alpha2, $uf_nome);
+//$stmt->store_result();
+//$stmt->bind_result($uf_id, $uf_alpha2, $uf_nome);
 
 while( $row = mysqli_fetch_array($stmt) ){
     $ufid = $row['uf_id'];
