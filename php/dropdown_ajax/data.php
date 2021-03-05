6edde0290abea8d6779c7fa8ca02279c15d57946
brunosacom <bruno.sa.com@gmail.com>
2021-03-05 11:48:32 -0300

    a

--- php/dropdown_ajax/data.php
+++ php/dropdown_ajax/data.php
@@ -3,7 +3,7 @@ require $_SERVER['DOCUMENT_ROOT']."/php/mysqli_connect.php";
 
 $paisid = $_GET['p'];   // pais id
 
-$sql = "SELECT uf_id, uf_alpha2, uf_nome FROM _uf WHERE pais_numero = ?";
+$sql = "SELECT uf_id, uf_alpha2, uf_nome FROM _uf WHERE pais_numero = $paisid";
 
 $result = mysqli_query($con,$sql);
 
