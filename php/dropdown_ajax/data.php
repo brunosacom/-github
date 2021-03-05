08f7ca2cceace07e52e4beaa3e86f5868b68824b
brunosacom <bruno.sa.com@gmail.com>
2021-03-05 11:37:33 -0300

    sa

--- php/dropdown_ajax/data.php
+++ php/dropdown_ajax/data.php
@@ -10,28 +10,24 @@ $sql = "SELECT uf_id, uf_alpha2, uf_nome FROM _uf WHERE pais_numero = ?";
 $stmt = $con->prepare($sql);
 $stmt->bind_param("s", $_GET['p']);
 $stmt->execute();
-//$stmt->store_result();
-//$stmt->bind_result($uf_id, $uf_alpha2, $uf_nome);
+$stmt->store_result();
+$stmt->bind_result($uf_id, $uf_alpha2, $uf_nome);
+$stmt->fetch();
+$stmt->close();
+
+
+
+//$uf_arr = array();
 
 while( $row = mysqli_fetch_array($stmt) ){
     $ufid = $row['uf_id'];
     $uf_nome = $row['uf_nome'];
 
     //$uf_arr[] = array("<option value='".$ufid."'>".$uf_nome."</option>");
-    echo "<option value=\"" . $ufid . "\">" . $uf_nome . "</option>";
+    echo "<option value=\"" . "teste " . $ufid . "\">" . $uf_nome . "</option>";
 }
 
-
-//$stmt->fetch();
-//$stmt->close();
-
-
-
-//$uf_arr = array();
-
-
-
-//echo "<option value=\"" . "teste " . $ufid . "\">" . $uf_nome . "</option>";
+echo "<option value=\"" . "teste " . $ufid . "\">" . $uf_nome . "</option>";
 // encoding array to json format
 //echo json_encode($uf_arr);
 
