2adb2c97c3a10fc430c0cb412a43abb242103af6
brunosacom <bruno.sa.com@gmail.com>
2021-03-24 11:16:07 -0300

    mun

--- php/dropdown_ajax/getmunicipio.php
+++ php/dropdown_ajax/getmunicipio.php
@@ -4,15 +4,15 @@ require $_SERVER['DOCUMENT_ROOT']."/php/mysqli_connect.php";
 $paisid = $_GET['ps'];   // pais id
 $ufid = $_GET['uf'];   // uf id
 
-$sql = "SELECT municipio_id, municipio_ibge, municipio_nome FROM _municipio WHERE pais_numero = $paisid AND uf_ibge = $ufid";
+$sql = "SELECT municipio_id, municipio_ibge, municipio_nome FROM _municipio WHERE uf_ibge = $ufid";
 
 $result = mysqli_query($con,$sql);
 
 while( $row = mysqli_fetch_array($result) ){
-    $ufid = $row['uf_id'];
-    $uf_nome = $row['uf_nome'];
+    $municipio_ibge = $row['municipio_ibge'];
+    $municipio_nome = $row['municipio_nome'];
 
-    echo "<option value=\"" . $ufid . "\">" . $uf_nome . "</option>";
+    echo "<option value=\"" . $municipio_ibge . "\">" . $municipio_nome . "</option>";
 }
 
 ?>
