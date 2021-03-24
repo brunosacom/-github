4072965b2f08de9a3a5a64d9c877a6464f49cc9a
brunosacom <bruno.sa.com@gmail.com>
2021-03-24 15:52:09 -0300

    inicial2

--- php/api_correios/index.php
+++ php/api_correios/index.php
@@ -1,126 +1,116 @@
-<!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Teste</title>
-<script src="https://code.jquery.com/jquery-3.5.1.js" type="text/javascript"></script>
-</head>
+    <head>
+    <title>ViaCEP Webservice</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<body>
+    <!-- Adicionando Javascript -->
+    <script>
+    
+    function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('logradouro').value=("");
+            document.getElementById('complemento').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('localidade').value=("");
+			document.getElementById('uf').value=("");
+			document.getElementById('unidade').value=("");
+			document.getElementById('ibge').value=("");
+			document.getElementById('gia').value=("");
+    }
 
-	<!--Formulário-->
-    <h1>Consulta ViaCEP JSON</h1>
-	<form>
-		<p>
-		  <label for="cep">CEP</label>
-		  <br>
-          <input id="cep" type="text" required/>
-	    </p>
-		<p>
-		  <label for="logradouro">Logradouro</label>
-		  <br>
-		  <input id="logradouro" type="text" required/></p>
-		<p>
-		  <label for="numero">Número</label>
-		  <br>
-		  <input id="numero" type="text" /></p>
-		<p>
-		  <label for="complemento">Complemento</label>
-		  <br>
-		  <input id="complemento" type="text"/>
-	    </p>
-		<p>
-		  <label for="bairro">Bairro</label>
-		  <br>
-		  <input id="bairro" type="text" required/>
-	    </p>
-      	<p>
-		  <label for="localidade">Localidade/Cidade</label>
-		  <br>
-		  <input id="localidade" type="text" required/>
-	    </p>
-		<p>
-		  <label for="uf">Estado</label>
-		  <br>
-		  <select id="uf">
-		    <option value="" selected></option>
-		    <option value="AC">Acre</option>
-		    <option value="AL">Alagoas</option>
-		    <option value="AP">Amapá</option>
-		    <option value="AM">Amazonas</option>
-		    <option value="BA">Bahia</option>
-		    <option value="CE">Ceará</option>
-		    <option value="DF">Distrito Federal</option>
-		    <option value="ES">Espírito Santo</option>
-		    <option value="GO">Goiás</option>
-		    <option value="MA">Maranhão</option>
-		    <option value="MT">Mato Grosso</option>
-		    <option value="MS">Mato Grosso do Sul</option>
-		    <option value="MG">Minas Gerais</option>
-		    <option value="PA">Pará</option>
-		    <option value="PB">Paraíba</option>
-		    <option value="PR">Paraná</option>
-		    <option value="PE">Pernambuco</option>
-		    <option value="PI">Piauí</option>
-		    <option value="RJ">Rio de Janeiro</option>
-		    <option value="RN">Rio Grande do Norte</option>
-		    <option value="RS">Rio Grande do Sul</option>
-		    <option value="RO">Rondônia</option>
-		    <option value="RR">Roraima</option>
-		    <option value="SC">Santa Catarina</option>
-		    <option value="SP">São Paulo</option>
-		    <option value="SE">Sergipe</option>
-		    <option value="TO">Tocantins</option>
-	      </select>
-	    </p>
-        <p>
-		  <label for="unidade">Unidade</label>
-		  <br>
-		  <input id="unidade" type="text"/>
-	    </p>
-        <p>
-		  <label for="ibge">IBGE - cod municipio</label>
-		  <br>
-		  <input id="ibge" type="text" required/>
-	    </p>
-        <p>
-		  <label for="gia">GIA</label>
-		  <br>
-		  <input id="gia" type="text"/>
-	    </p>
-    </form>
-	
-	<script type="text/javascript">
-		$("#cep").focusout(function(){
-			//Início do Comando AJAX
-			$.ajax({
-				//O campo URL diz o caminho de onde virá os dados
-				//É importante concatenar o valor digitado no CEP
-				url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/unicode/',
-				//Aqui você deve preencher o tipo de dados que será lido,
-				//no caso, estamos lendo JSON.
-				dataType: 'json',
-				//SUCESS é referente a função que será executada caso
-				//ele consiga ler a fonte de dados com sucesso.
-				//O parâmetro dentro da função se refere ao nome da variável
-				//que você vai dar para ler esse objeto.
-				success: function(resposta){
-					//Agora basta definir os valores que você deseja preencher
-					//automaticamente nos campos acima.
-					$("#logradouro").val(resposta.logradouro);
-					$("#complemento").val(resposta.complemento);
-					$("#bairro").val(resposta.bairro);
-					$("#localidade").val(resposta.localidade);
-					$("#uf").val(resposta.uf);
-					$("#unidade").val(resposta.unidade);
-					$("#ibge").val(resposta.ibge);
-					$("#gia").val(resposta.gia);
-					//Vamos incluir para que o Número seja focado automaticamente
-					//melhorando a experiência do usuário
-					$("#numero").focus();
-				}
-			});
-		});
-	</script>
-</body>
-</html>
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('logradouro').value=(conteudo.logradouro);
+			document.getElementById('complemento').value=(conteudo.complemento);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+			document.getElementById('unidade').value=(conteudo.unidade);
+            document.getElementById('ibge').value=(conteudo.ibge);
+			document.getElementById('gia').value=(conteudo.gia);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+        
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementById('logradouro').value="...";
+				document.getElementById('complemento').value="...";
+                document.getElementById('bairro').value="...";
+                document.getElementById('cidade').value="...";
+                document.getElementById('uf').value="...";
+				document.getElementById('unidade').value="...";
+                document.getElementById('ibge').value="...";
+				document.getElementById('gia').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
+
+    </script>
+    </head>
+
+    <body>
+    <!-- Inicio do formulario -->
+      <form method="get" action=".">
+        <label>Cep:
+        <input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
+               onblur="pesquisacep(this.value);" /></label><br />
+        <label>logradouro:
+        <input name="logradouro" type="text" id="logradouro" size="60" /></label><br />
+		<label>numero:
+        <input name="numero" type="text" id="numero" size="60" /></label><br />
+		<label>complemento:
+        <input name="complemento" type="text" id="complemento" size="60" /></label><br />
+        <label>Bairro:
+        <input name="bairro" type="text" id="bairro" size="40" /></label><br />
+        <label>Cidade:
+        <input name="cidade" type="text" id="cidade" size="40" /></label><br />
+        <label>Estado:
+        <input name="uf" type="text" id="uf" size="2" /></label><br />
+		<label>unidade:
+        <input name="unidade" type="text" id="unidade" size="60" /></label><br />
+        <label>IBGE:
+        <input name="ibge" type="text" id="ibge" size="8" /></label><br />
+		<label>GIA:
+        <input name="gia" type="text" id="gia" size="60" /></label><br />
+      </form>
+    </body>
+
+    </html>
