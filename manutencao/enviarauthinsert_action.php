50890837d4fe9c5873e67f48b6116df61df5179c
brunosacom <bruno.sa.com@gmail.com>
2022-07-27 18:02:03 -0300

    phpmailler

--- manutencao/enviarauthinsert_action.php
+++ manutencao/enviarauthinsert_action.php
@@ -1,116 +1,159 @@
-<?php require $_SERVER['DOCUMENT_ROOT'].'/php/mysqli_connect.php'; //MySqlDB connect brunosacom ?>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/php/mysqli_connect.php'; //MySqlDB connect brunosacom 
+?>
 
 <!DOCTYPE HTML>
 <html lang="pt-BR">
+
 <head>
-        <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/analyticstracking.php'; ?>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?php echo $empresa_nome; ?> - Manutenção</title>
-        <link href="https://fonts.googleapis.com/css?family=Didact Gothic" rel="stylesheet">
-        <?php include_once $_SERVER['DOCUMENT_ROOT'].'/php/bootstrap_head.php'; ?>
-    </head>
+  <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/php/analyticstracking.php'; ?>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php echo $empresa_nome; ?> - Manutenção</title>
+  <link href="https://fonts.googleapis.com/css?family=Didact Gothic" rel="stylesheet">
+  <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/php/bootstrap_head.php'; ?>
+</head>
 
-    <body style="font-family: Didact Gothic;">
-        <div class="container">
-        <!-- Content here -->
-    
-<?php
+<body style="font-family: Didact Gothic;">
+  <div class="container">
+    <!-- Content here -->
 
-if(isset($_POST['submit'])) {
+    <?php
+    // inclui as classes do PHPMailer
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-$charset = $_POST['charset'];
-$manutencao_status = addslashes($_POST['manutencao_status']);
-$manutencao_email = addslashes($_POST['manutencao_email']);
-$manutencao_cinema = addslashes($_POST['manutencao_cinema']);
-$manutencao_area = addslashes($_POST['manutencao_area']);
-$manutencao_gerente = addslashes($_POST['manutencao_gerente']);
-$manutencao_tiposolicitacao = addslashes($_POST['manutencao_tiposolicitacao']);
-$manutencao_urgencia = addslashes($_POST['manutencao_urgencia']);
-$manutencao_relateproblema = addslashes($_POST['manutencao_relateproblema']);
-$manutencao_localizacao = addslashes($_POST['manutencao_localizacao']);
+    if (isset($_POST['submit'])) {
 
-
-
-$caixaPostalServidorNome = "".$manutencao_cinema." Manutencao";
-$caixaPostalServidorEmail = 'formulario@grupoestacao.com.br';
-$caixaPostalServidorSenha = 'Estacao12345';
-
-$assunto = "[[".$manutencao_cinema." - ".$manutencao_area." - ".$manutencao_tiposolicitacao."]]";
-
-$mensagemConcatenada = 'LOCALIZACAO'.'<br/>'; 
-$mensagemConcatenada .= 'Cinema: '.$manutencao_cinema.'<br/>';
-$mensagemConcatenada .= 'Area: '.$manutencao_area.'<br/>';
-$mensagemConcatenada .= 'Localizacao: '.$manutencao_localizacao.'<br/>';
-$mensagemConcatenada .= 'Tipo: '.$manutencao_tiposolicitacao.'<br/><br/>';
-
-$mensagemConcatenada .= 'DESCRICAO'.'<br/>';
-$mensagemConcatenada .= 'Relato: '.$manutencao_relateproblema.'<br/><br/>';
-$mensagemConcatenada .= 'Status: '.$manutencao_status.'<br/><br/>';
-
-$mensagemConcatenada .= 'RESPONSAVEL'.'<br/>';
-$mensagemConcatenada .= 'Gerente: '.$manutencao_gerente.'<br/><br/>';
+      $charset = $_POST['charset'];
+      $manutencao_status = addslashes($_POST['manutencao_status']);
+      $manutencao_email = addslashes($_POST['manutencao_email']);
+      $manutencao_cinema = addslashes($_POST['manutencao_cinema']);
+      $manutencao_area = addslashes($_POST['manutencao_area']);
+      $manutencao_gerente = addslashes($_POST['manutencao_gerente']);
+      $manutencao_tiposolicitacao = addslashes($_POST['manutencao_tiposolicitacao']);
+      $manutencao_urgencia = addslashes($_POST['manutencao_urgencia']);
+      $manutencao_relateproblema = addslashes($_POST['manutencao_relateproblema']);
+      $manutencao_localizacao = addslashes($_POST['manutencao_localizacao']);
 
 
-/*********************************** A PARTIR DAQUI NAO ALTERAR ************************************/
+      $remetente = "formulario@bruno-sa.com"; // INSIRA AQUI UM EMAIL CRIADO EM SUA HOSPEDAGEM PARA QUE A MENSAGEM SEJA ENVIADA CORRETAMENTE.
+      $nomeRemetente = "Form BEMBOS Manutenção"; // INSIRA AQUI O NOME PARA APARECER NO CAMPO FROM.
+      $destinatario = "bruno" . "@bruno-sa.com"; // INSIRA AQUI O ENDEREÇO DO DESTINATÁRIO DO E-MAIL.
+      $assunto = "[[" . $manutencao_cinema . " - " . $manutencao_area . " - " . $manutencao_tiposolicitacao . "]]" . "\r\n";
 
- 
+      $corpo =
+        "LOCALIZACAO" . "<br/>" .
+        "Cinema: " . $manutencao_cinema . "<br/>" .
+        "Area: " . $manutencao_area . "<br/>" .
+        "Localizacao: " . $manutencao_localizacao . "<br/>" .
+        "Tipo: " . $manutencao_tiposolicitacao . "<br/><br/>" .
 
-require_once('../PHPMailer-master/PHPMailerAutoload.php');
+        "DESCRICAO" . "<br/>" .
+        "Relato: " . $manutencao_relateproblema . "<br/><br/>" .
 
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->SMTPAuth  = true;
-$mail->Charset   = 'utf8';
-$mail->Host  = 'smtp.'.substr(strstr($caixaPostalServidorEmail, '@'), 1);
-$mail->Port  = '587';
-$mail->Username  = $caixaPostalServidorEmail;
-$mail->Password  = $caixaPostalServidorSenha;
-$mail->From  = $caixaPostalServidorEmail;
-$mail->FromName  = $caixaPostalServidorNome;
-$mail->IsHTML(true);
-$mail->Subject  = $assunto;
-$mail->Body  = $mensagemConcatenada;
+        "Status: " . $manutencao_status . "<br/><br/>" .
 
-/*********************************** EMAILS QUE RECEBEM O FORM ************************************/
+        "RESPONSAVEL" . "<br/>" .
+        "Gerente: " . $manutencao_gerente . "<br/><br/>" . "\r\n";
 
 
-$mail->AddAddress('bruno@bruno-sa.com', 'Manutencao RJ');
+      $headers = "MIME-Version: 1.0\r\n";
+      $headers .= "Content-type: text/html; charset=" . $charset . "\r\n";
+      $headers .= "Reply-To: " . $manutencao_email . "\r\n";
+      //$headers .= "Cc: copia@seudominio\r\n"; //CAMPO COPIA OPCIONAL
+      //$headers .= "Bcc: copiaoculta@seudominio\r\n"; //CAMPO COPIA OCULTA OPCIONAL
+      $headers .= "From: " . $remetente . "\r\n";
 
-$mail->AddReplyTo($manutencao_email, $manutencao_cinema);
+      //SMTP needs accurate times, and the PHP time zone MUST be set
+      //This should be done in your php.ini, but this is how to do it if you don't have access to that
+      //date_default_timezone_set('Etc/UTC');
 
-
-if(!$mail->Send()){
-
-echo '<p><b> Erro ao enviar formulário: '. print($mail->ErrorInfo);
-
-}else{
-
-echo 'Mail enviado com sucesso!<br/>';
-
-} 
+      require $_SERVER['DOCUMENT_ROOT'] . '/_PHPMailer-master/src/PHPMailer.php';
+      require $_SERVER['DOCUMENT_ROOT'] . '/_PHPMailer-master/src/SMTP.php';
+      require $_SERVER['DOCUMENT_ROOT'] . '/_PHPMailer-master/src/Exception.php';
 
 
 
-}
+      //Create a new PHPMailer instance
+      $$mail = new PHPMailer(true);
+
+      //Define Charset
+      $mail->CharSet = 'UTF-8';
+
+      //Tell PHPMailer to use SMTP
+      $mail->isSMTP();
+
+      //Enable SMTP debugging
+      // 0 = off (for production use)
+      // 1 = client messages
+      // 2 = client and server messages
+      $mail->SMTPDebug = 0;
+
+      //Ask for HTML-friendly debug output
+      $mail->Debugoutput = 'html';
+
+      // servidor SMTP
+      $mail->Host = $empresa_smtp;
+
+      // usuário, senha e porta do SMTP
+      $mail->Username = $empresa_username;
+      $mail->Password = $empresa_password;
+      $mail->Port = 587;
+
+      //Set the encryption system to use - ssl (deprecated) or tls
+      //$mail->SMTPSecure = 'tls';
+
+      //Whether to use SMTP authentication
+      $mail->SMTPAuth = true;
+
+      //Set who the message is to be sent from
+      $mail->setFrom($remetente, $nomeRemetente);
+
+      //Set an alternative reply-to address
+      $mail->addReplyTo($manutencao_email, $manutencao_gerente);
+
+      //Set who the message is to be sent to
+      $mail->addAddress($destinatario, $emp_sigla);
+
+      //Set the subject line
+      $mail->Subject = $assunto;
+
+      //Read an HTML message body from an external file, convert referenced images to embedded,
+      //convert HTML into a basic plain-text alternative body
+      $mail->msgHTML($corpo);
+
+      //Replace the plain text body with one created manually
+      $mail->AltBody = $corpo;
+
+      //Attach an image file
+      //$mail->addAttachment('images/phpmailer_mini.png');
+
+      //send the message, check for errors
+      if (!$mail->send()) {
+        //    echo "x" . $mail->ErrorInfo;
+      } else {
+        echo "@";
+      }
+    }
 
 
-$sql="INSERT INTO bembos_manutencao (charset, manutencao_cinema, manutencao_urgencia, manutencao_area, manutencao_gerente, manutencao_tiposolicitacao, manutencao_relateproblema, manutencao_localizacao, manutencao_status, manutencao_email)
+    $sql = "INSERT INTO bembos_manutencao (charset, manutencao_cinema, manutencao_urgencia, manutencao_area, manutencao_gerente, manutencao_tiposolicitacao, manutencao_relateproblema, manutencao_localizacao, manutencao_status, manutencao_email)
 VALUES
 ('$charset','$manutencao_cinema','$manutencao_urgencia','$manutencao_area','$manutencao_gerente','$manutencao_tiposolicitacao','$manutencao_relateproblema','$manutencao_localizacao','$manutencao_status','$manutencao_email')";
 
-if (!mysqli_query($con, $sql))
-  {
-  die('Error: ' . mysqli_error($con));
-  }
-echo "<br>Obrigado por enviar a solicitacao/manutencao de ".$manutencao_cinema.".<br>";
+    if (!mysqli_query($con, $sql)) {
+      die('Error: ' . mysqli_error($con));
+    }
+    echo "<br>Obrigado por enviar a solicitacao/manutencao de " . $manutencao_cinema . ".<br>";
 
 
 
-mysqli_close($con)
-?>
+    mysqli_close($con)
+    ?>
 
 
 </body>
+
 </html>
