c93ad50f1c4889e2d7e91c5cc7bd3cf0edc78ba4
brunosacom <bruno.sa.com@gmail.com>
2022-07-30 17:48:28 -0300

    bootstrap

--- manutencao/consultalistagem_selectwhere_status_color.php
+++ manutencao/consultalistagem_selectwhere_status_color.php
@@ -53,10 +53,10 @@
                     <small>Legenda Status
                         <small>
                             <div>
-                                FUNDO: <span class="bg-light">BRANCO - NORMAL</span> |
-                                <span class="bg-warning">AMARELO - URGENTE</span> |
-                                <span class="bg-danger">VERMELHO - URGENCIA MAXIMA</span> |
-                                <span class="bg-success">VERDE - ADIAVEL</span>
+                                FUNDO: <span class="bg-light text-body">BRANCO - NORMAL</span> |
+                                <span class="bg-warning text-body">AMARELO - URGENTE</span> |
+                                <span class="bg-danger text-body">VERMELHO - URGENCIA MAXIMA</span> |
+                                <span class="bg-success text-body">VERDE - ADIAVEL</span>
                             </div>
                             <div>
                                 TEXTO: <span class="bg-light text-body">PRETO - ABERTO</span> |
