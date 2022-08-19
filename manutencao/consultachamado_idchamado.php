6fa113573d1aa0faee8488edf815ad64da798230
brunosacom <bruno.sa.com@gmail.com>
2022-08-19 08:01:42 -0300

    layoput

--- manutencao/consultachamado_idchamado.php
+++ manutencao/consultachamado_idchamado.php
@@ -95,8 +95,8 @@
                     </small>
                     <hr>
                 </div>
-                <div><small>ID PEDIDO: </small><?php echo $id_manutencao . " " . $manutencao_urgencia ?></div>
-                <div><small>DATA e HORA abertura: </small><?php echo $manutencao_timestamp ?><br></div>
+                <div><small>ID PEDIDO: </small><?php echo $id_manutencao ?> <span class="bg-danger text-body"><small><?php echo $manutencao_urgencia ?></small></span></div>
+                <div><small>DATA e HORA abertura / modificação: </small><?php echo $manutencao_timestamp ?> / <?php echo $manutencao_updatetimestamp ?><br></div>
                 <div><small>ANDAMENTO: </small><?php echo $manutencao_andamento ?></div>
                 <div>AREA</div>
                 <div><?php echo $manutencao_area ?><br></div>
@@ -107,8 +107,7 @@
                 <div>RELATO</div>
                 <div><?php echo $manutencao_relateproblema ?><br></div>
                 
-                <div>DATA e HORA modificação</div>
-                <div><?php echo $manutencao_updatetimestamp ?></div>
+                
 
 
 
